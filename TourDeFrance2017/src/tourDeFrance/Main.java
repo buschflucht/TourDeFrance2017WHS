5e87cb0e81a94c92e1dea33d69e46852104eff14
julian <julian@DESKTOP-6DKA0U5.za.w-hs.de>
2017-06-27 14:34:53 +0200

    Konstruktor erstellt für EtappenView

--- TourDeFrance2017/src/tourDeFrance/Main.java
+++ TourDeFrance2017/src/tourDeFrance/Main.java
@@ -3,14 +3,25 @@ package tourDeFrance;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import tourDeFrance.model.Etappen;
 
 public class Main extends Application {
 	private Stage primaryStage;
+	
+	 private ObservableList<Etappen> EtappenData = FXCollections.observableArrayList();
 
+	 
+	 public Main(){
+		 
+	 }
+	 
+	 
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
--- TourDeFrance2017/src/tourDeFrance/model/Etappen.java
+++ TourDeFrance2017/src/tourDeFrance/model/Etappen.java
@@ -2,17 +2,23 @@ package tourDeFrance.model;
 
 import java.sql.Time;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Etappen {
 
 	private IntegerProperty etappenID;
 	private IntegerProperty etappennummer;
-	private ObjectProperty<LocalDate> datum;
+	private ObjectProperty<LocalDateTime> datum;
 	private StringProperty startort;
 	private StringProperty zielort;
 	private DoubleProperty laenge;
@@ -29,65 +35,90 @@ public class Etappen {
 	private StringProperty fahrerBerg;
 	private StringProperty dopingFahrer;
 	private StringProperty dopingTeam;
+
+	public Etappen(Integer etappenID, Date datum, Time uhrzeit, String startort, String zielort, Double laenge) {
+		this.etappenID = new SimpleIntegerProperty(etappenID);
+		this.datum = new SimpleObjectProperty<LocalDateTime>();
+		this.startort = new SimpleStringProperty(startort);
+		this.zielort = new SimpleStringProperty(zielort);
+		this.laenge = new SimpleDoubleProperty(laenge);
+		
+	}
+
 	public IntegerProperty getEtappenID() {
 		return etappenID;
 	}
+
 	public IntegerProperty getEtappennummer() {
 		return etappennummer;
 	}
-	public ObjectProperty<LocalDate> getDatum() {
+
+	public ObjectProperty<LocalDateTime> getDatum() {
 		return datum;
 	}
+
 	public StringProperty getStartort() {
 		return startort;
 	}
+
 	public StringProperty getZielort() {
 		return zielort;
 	}
+
 	public DoubleProperty getLaenge() {
 		return laenge;
 	}
+
 	public IntegerProperty getArt() {
 		return art;
 	}
+
 	public StringProperty getFahrerPlatz1() {
 		return fahrerPlatz1;
 	}
+
 	public Time getSiegerzeit() {
 		return siegerzeit;
 	}
+
 	public StringProperty getFahrerPlatz2() {
 		return fahrerPlatz2;
 	}
+
 	public StringProperty getFahrerPlatz3() {
 		return fahrerPlatz3;
 	}
+
 	public StringProperty getTeamPlatz1() {
 		return teamPlatz1;
 	}
+
 	public StringProperty getTeamPlatz2() {
 		return teamPlatz2;
 	}
+
 	public StringProperty getTeamPlatz3() {
 		return teamPlatz3;
 	}
+
 	public StringProperty getFahrerGelb() {
 		return fahrerGelb;
 	}
+
 	public StringProperty getFahrerGruen() {
 		return fahrerGruen;
 	}
+
 	public StringProperty getFahrerBerg() {
 		return fahrerBerg;
 	}
+
 	public StringProperty getDopingFahrer() {
 		return dopingFahrer;
 	}
+
 	public StringProperty getDopingTeam() {
 		return dopingTeam;
 	}
-	
-	
-	
 
 }
