0d965712c439329132bf3c13ca940ee17c465f21
julian <julian@DESKTOP-6DKA0U5.za.w-hs.de>
2017-06-27 13:36:21 +0200

    j

--- TourDeFrance2017/src/tourDeFrance/model/Teams.java
+++ TourDeFrance2017/src/tourDeFrance/model/Teams.java
@@ -5,6 +5,15 @@ public class Teams {
 	private int teamID;
 	private String teamName;
 	private String teamBildUrl;
+	public int getTeamID() {
+		return teamID;
+	}
+	public String getTeamName() {
+		return teamName;
+	}
+	public String getTeamBildUrl() {
+		return teamBildUrl;
+	}
 	
 	
 }
--- TourDeFrance2017/src/tourDeFrance/model/Tipps.java
+++ TourDeFrance2017/src/tourDeFrance/model/Tipps.java
@@ -16,6 +16,48 @@ public class Tipps {
 	private String fahrerBerg;
 	private String fahrerDoping;
 	private String teamDoping;
+	public int getTippID() {
+		return tippID;
+	}
+	public int getUserID() {
+		return userID;
+	}
+	public int getEtappenID() {
+		return etappenID;
+	}
+	public String getFahrerPlatz1() {
+		return fahrerPlatz1;
+	}
+	public String getFahrerPlatz2() {
+		return fahrerPlatz2;
+	}
+	public String getFahrerPlatz3() {
+		return fahrerPlatz3;
+	}
+	public String getTeamPlatz1() {
+		return teamPlatz1;
+	}
+	public String getTeamPlatz2() {
+		return teamPlatz2;
+	}
+	public String getTeamPlatz3() {
+		return teamPlatz3;
+	}
+	public String getFahrerGelb() {
+		return fahrerGelb;
+	}
+	public String getFahrerGruen() {
+		return fahrerGruen;
+	}
+	public String getFahrerBerg() {
+		return fahrerBerg;
+	}
+	public String getFahrerDoping() {
+		return fahrerDoping;
+	}
+	public String getTeamDoping() {
+		return teamDoping;
+	}
 
 	
 	
